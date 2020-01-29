bed2f164da628fe10f68d118b4f3428e252d29aa
UnsignedInt8 <unsignedint8@gmail.com>
2020-01-29 22:21:08 +0800

    [add] contracts/erc20abi

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Metamask from './lib/Metamask';
 import Payment from './lib/Payment';
+import Contracts from './lib/Contracts';
+import Erc20Abi from './lib/ERC20Abi';
 
-export { Metamask, Payment };
+export { Metamask, Payment, Contracts, Erc20Abi };
