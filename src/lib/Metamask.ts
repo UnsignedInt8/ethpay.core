5da7d6611eb96a96c1a9bf09947d6d34ae9de49a
UnsignedInt8 <unsignedint8@gmail.com>
2020-02-12 16:34:27 +0800

    [code] add provider

--- src/lib/Metamask.ts
+++ src/lib/Metamask.ts
@@ -1,83 +1,104 @@
 export default class Metamask {
-    public static hasWeb3() {
-        return typeof window?.['ethereum'] !== 'undefined';
+  public static hasWeb3() {
+    return typeof window?.['ethereum'] !== 'undefined';
+  }
+
+  public static hasMetamask() {
+    return window?.['ethereum']?.['isMetamask'] ?? false;
+  }
+
+  static getProvider() {
+    return window?.['ethereum'] || window?.['web3'];
+  }
+
+  public static async enable() {
+    return await this.requestAccounts();
+  }
+
+  /**
+   * @returns Returns an array of accounts, currently only one account would be returned.
+   */
+  public static async requestAccounts() {
+    try {
+      // For old metamask
+      const accounts = await window?.['ethereum'].enable();
+      return accounts as string[];
+    } catch (error) {}
+
+    try {
+      // You now have an array of accounts!
+      // Currently only ever one:
+      // ['0xFDEa65C8e26263F6d9A1B5de9555D2931A33b825']
+      const accounts = await window?.['ethereum'].send('eth_requestAccounts');
+      return accounts as string[];
+    } catch (error) {
+      return [];
     }
+  }
 
-    public static hasMetamask() {
-        return window?.['ethereum']?.['isMetamask'] ?? false;
-    }
-
-    public static async enable() {
-        return await this.requestAccounts();
-    }
-
-    /**
-     * @returns Returns an array of accounts, currently only one account would be returned.
-     */
-    public static async requestAccounts() {
-        try {
-            // For old metamask
-            const accounts = await window?.['ethereum'].enable();
-            return accounts as string[];
-        } catch (error) { }
-
-        try {
-            // You now have an array of accounts!
-            // Currently only ever one:
-            // ['0xFDEa65C8e26263F6d9A1B5de9555D2931A33b825']
-            const accounts = await window?.['ethereum'].send('eth_requestAccounts')
-            return accounts as string[];
-        } catch (error) {
-            return [];
-        }
-    }
-
-    /**
-     * Send transaction
-     * @param params  
-     * @returns Returns a transaction hash if succeeded, empty string if failed.
-     */
-    public static async sendTransaction(params: { to: string, from: string, gas?: string, gasPrice?: string, value?: string, data?: string }) {
-        return new Promise<string>(resolve => {
-            try {
-                window?.['ethereum']?.send({ method: 'eth_sendTransaction', params: [params], from: params.from }, (err: any, result: any) => {
-                    if (err) {
-                        resolve('');
-                        return;
-                    }
-
-                    resolve(result?.result ?? '');
-                });
-            } catch (error) {
-                resolve('');
+  /**
+   * Send transaction
+   * @param params
+   * @returns Returns a transaction hash if succeeded, empty string if failed.
+   */
+  public static async sendTransaction(params: {
+    to: string;
+    from: string;
+    gas?: string;
+    gasPrice?: string;
+    value?: string;
+    data?: string;
+  }) {
+    return new Promise<string>(resolve => {
+      try {
+        window?.['ethereum']?.send(
+          {
+            method: 'eth_sendTransaction',
+            params: [params],
+            from: params.from
+          },
+          (err: any, result: any) => {
+            if (err) {
+              resolve('');
+              return;
             }
-        });
+
+            resolve(result?.result ?? '');
+          }
+        );
+      } catch (error) {
+        resolve('');
+      }
+    });
+  }
+
+  /**
+   * Get the balance of specified address
+   * @param address
+   * @returns Balance hex string in wei
+   */
+  public static async getBalance(address?: string) {
+    let addr = address ?? (await this.requestAccounts()[0]);
+    if (!addr) {
+      return '0x00';
     }
 
-    /**
-     * Get the balance of specified address
-     * @param address 
-     * @returns Balance hex string in wei
-     */
-    public static async getBalance(address?: string) {
-        let addr = address ?? await this.requestAccounts()[0];
-        if (!addr) {
-            return '0x00';
-        }
-
-        return new Promise<string>(resolve => {
-            try {
-                window?.['ethereum']?.send({ method: 'eth_getBalance', params: [addr, 'latest'] }, (err: any, result: any) => {
-                    if (err) {
-                        resolve('0x00');
-                        return;
-                    }
-
-                    resolve(result?.result ?? '0x00');
-                })
-            } catch{
-                resolve('0x00');
+    return new Promise<string>(resolve => {
+      try {
+        window?.['ethereum']?.send(
+          { method: 'eth_getBalance', params: [addr, 'latest'] },
+          (err: any, result: any) => {
+            if (err) {
+              resolve('0x00');
+              return;
             }
-        });
-    }
+
+            resolve(result?.result ?? '0x00');
+          }
+        );
+      } catch {
+        resolve('0x00');
+      }
+    });
+  }
 }
