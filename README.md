4e4366e31930351ec900818e9980a4bedd0671c8
UnsignedInt8 <unsignedint8@gmail.com>
2020-05-09 10:20:29 +0800

    Remove hard-coded gas

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 ETHPay core lib for ETHPay React/Vue/Angular
 
+## Quick Start
+
+Sending Stablecoins
+
+```javascript
+import { Payment } from 'ethpay';
+
+const txHash = await Payment.send({ to: '0x123456...', value: 100, currency: 'dai' });
+```
--- src/lib/Payment.ts
+++ src/lib/Payment.ts
@@ -1,75 +1,80 @@
-
-import * as ethers from 'ethers';
-import ERC20Abi from './ERC20Abi';
-import Metamask from './Metamask';
-import Contracts from './Contracts';
+import * as ethers from "ethers";
+import ERC20Abi from "./ERC20Abi";
+import Metamask from "./Metamask";
+import Contracts from "./Contracts";
 
 /**
  * High level utility class
  */
 export default class Payment {
-    /**
-     * build ERC20 transfering ABI
-     * @param to receiptor address
-     * @param amount wei/min usd unit
-     * @returns hex ABI string
-     */
-    public static buildErc20TransferABI(to: string, value: string) {
-        const coder = new ethers.utils.AbiCoder();
-        const amount = coder.encode(['uint256'], [value]);
-        const erc20 = new ethers.utils.Interface(ERC20Abi);
-        const abi = erc20.functions.transfer.encode([to, amount]);
-        return abi;
+  /**
+   * build ERC20 transfering ABI
+   * @param to receiptor address
+   * @param amount wei/min usd unit
+   * @returns hex ABI string
+   */
+  public static buildErc20TransferABI(to: string, value: string) {
+    const coder = new ethers.utils.AbiCoder();
+    const amount = coder.encode(["uint256"], [value]);
+    const erc20 = new ethers.utils.Interface(ERC20Abi);
+    const abi = erc20.functions.transfer.encode([to, amount]);
+    return abi;
+  }
+
+  public static parseUnits(value: string, unit: string | number) {
+    return ethers.utils.parseUnits(value, unit);
+  }
+
+  /**
+   * Send ETH tx
+   * @param param
+   * @returns Tx hash, or empty string if failed
+   */
+  public static async send({
+    to,
+    value,
+    currency,
+    data
+  }: {
+    to: string;
+    value?: number | string;
+    data?: string;
+    currency: "eth" | "usdc" | "usdt" | "dai" | "sai" | "pax" | "tusd" | "gusd" | "usdx" | "husd" | "usdk";
+  }) {
+    if (!Metamask.hasWeb3()) {
+      return "";
     }
 
-    public static parseUnits(value: string, unit: string | number) {
-        return ethers.utils.parseUnits(value, unit);
+    let [from] = await Metamask.enable();
+    if (!from) return "";
+
+    let hash: string;
+
+    if (currency === "eth") {
+      hash = await Metamask.sendTransaction({
+        to,
+        from,
+        value: ethers.utils.parseEther(`${value || 0}`).toHexString(),
+        data
+      });
+    } else {
+      const contract = Contracts[currency] as {
+        addr: string;
+        decimals: number;
+      };
+
+      if (!contract) {
+        return "";
+      }
+
+      hash = await Metamask.sendTransaction({
+        to: contract.addr,
+        from,
+        value: "0x00",
+        data: Payment.buildErc20TransferABI(to, ethers.utils.parseUnits(`${value || 0}`, contract.decimals).toHexString())
+      });
     }
 
-    /**
-     * Send ETH tx
-     * @param param 
-     * @returns Tx hash, or empty string if failed
-     */
-    public static async send({ to, value, currency, data }: { to: string, value?: number | string, data?: string, currency: 'eth' | 'usdc' | 'usdt' | 'dai' | 'sai' | 'pax' | 'tusd' | 'gusd' | 'usdx' | 'husd' | 'usdk' }) {
-        if (!Metamask.hasWeb3()) {
-            return '';
-        }
-
-        let [from] = await Metamask.enable();
-        if (!from) return '';
-
-        let hash: string;
-
-        if (currency === "eth") {
-            hash = await Metamask.sendTransaction({
-                to,
-                from,
-                gas: "0x5208",
-                value: ethers.utils.parseEther(`${value || 0}`).toHexString(),
-                data
-            });
-        } else {
-            const contract = Contracts[currency] as { addr: string, decimals: number };
-
-            if (!contract) {
-                return '';
-            }
-
-            hash = await Metamask.sendTransaction({
-                to: contract.addr,
-                from,
-                gas: "0x186A0",
-                value: "0x00",
-                data: Payment.buildErc20TransferABI(
-                    to,
-                    ethers.utils
-                        .parseUnits(`${value || 0}`, contract.decimals)
-                        .toHexString()
-                )
-            });
-        }
-
-        return hash;
-    }
+    return hash;
+  }
 }
